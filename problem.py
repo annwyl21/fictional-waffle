368e7038226259edc7bb5daec2fbbea31fe00dd6
Ellen Houghton <ellen.a.ash@gmail.com>
2023-02-09 11:26:35 +0000

    struggling to get the input

---
+++ problem.py
@@ -0,0 +1,28 @@
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+)
+
+# that is the given code starter, i can tell it is part of a bigger structure because it has a random ) floating at the bottom.
+# But this is supposed to be the simple, 'we have given you the code for the input now grab the data and start coding'
+
+# except, i have no idea how to use this to grab the data!
+
+# I can't print it to view the data structure I am receiving
+
+#I should be able to grab something like this...
+"5"
+'Harry'
+'37.21'
+'Berry'
+'37.21'
+'Tina'
+'37.2'
+'Akriti'
+'41'
+'Harsh'
+'39'
+
+#but I can only get
+'Harry' and '37.21'
